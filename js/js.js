99f105b2959cf0332d3524ca1b76903cafd61831
remhon14 <remhon8@gamil.com>
2025-06-16 14:42:00 +0700

    JavaScript

---
+++ js/js.js
@@ -0,0 +1,44 @@
+
+
+// const add = () =>{
+//     return 1+2;
+// }
+// console.log(add())
+
+// add= [12,5,6,78,20]
+// console.log(add)
+
+
+// const a = (x,y) =>{
+//     return 1+2;
+// }
+// console.log(a())
+
+
+
+
+// const b = (x,y) =>{
+//     return 5-2;
+// }
+// console.log(b())
+
+
+
+// const c = (x,y) =>{
+//     return 2*5;
+// }
+
+// console.log(c())
+
+// const d = (x,y) =>{
+//     return 10/2;
+// }
+
+// console.log(d())
+
+
+
+
+// Lambda Expression
+const greeting = name => console.log(`Hello ${name}`)
+console.log(greeting())
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const c = (x,y) =>{
+//     return 2*5;
+// }
+
+// console.log(v())
+
+// const d = (x,y) =>{
